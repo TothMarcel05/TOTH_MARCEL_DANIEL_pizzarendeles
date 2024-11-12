45c88309735bee57f78918afb4dd3056b3300c4e
TothMarcel05 <toth.marcel.daniel@diak.szbi-pg.hu>
2024-11-12 10:36:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,28 +9,26 @@ def index():
 
 @app.route('/pizza-order', methods=["GET", "POST"])
 def pizza_order():
-        if request.method == 'POST':
-    return render_template('index.html')
-    _name = request.form.get('name')
-    _phone = request.form.get('phone')
-    _size = request.form.get('size')
-    _ham = request.form.get('ham')
-    _cheese = request.form.get('cheese')
-    _mushroom = request.form.get('mushroom')
-    _measure = request.form.get('measure')
-    _time = request.form.get('time')
-    _more = request.form.get('more')
-    order ={
-        'name': _name,
-        'phone': _phone,
-        'size': _size,
-        'ham': _ham,
-        'cheese': _cheese,
-        'mushroom': _mushroom,
-        'measure': _measure,
-        'time': _time,
-        'more': _more}
-
+    if request.method == 'POST':
+        _name = request.form.get('name')
+        _phone = request.form.get('phone')
+        _size = request.form.get('size')
+        _ham = request.form.get('ham')
+        _cheese = request.form.get('cheese')
+        _mushroom = request.form.get('mushroom')
+        _measure = request.form.get('measure')
+        _time = request.form.get('time')
+        _more = request.form.get('more')
+        order ={
+            'name': _name,
+            'phone': _phone,
+            'size': _size,
+            'ham': _ham,
+            'cheese': _cheese,
+            'mushroom': _mushroom,
+            'measure': _measure,
+            'time': _time,
+            'more': _more}
         orders.append(order)
         print(orders)
     return render_template('pizza_order.html')
