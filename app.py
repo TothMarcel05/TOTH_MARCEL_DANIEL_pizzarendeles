9564fb64c62a602f74582a53590c36480ae842d6
TothMarcel05 <toth.marcel.daniel@diak.szbi-pg.hu>
2024-11-12 09:31:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/pizza-order', methods=["GET"])
+def pizza_order():
+    return render_template('pizza_order.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
